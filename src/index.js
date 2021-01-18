a3eeb52ab83040797fa40633d92e937a0036232b
damianberger <62711233+damianberger@users.noreply.github.com>
2021-01-18 15:21:02 +0100

    store provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
+import {Provider} from "react-redux"
+
+import store from './reducers/store'
 import App from './components/App';
 
+import './styles/index.css';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+document.getElementById('root')
 );
